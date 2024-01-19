ef8bbb9bc882a2705ddc9e2288e5e65d1e3cb057
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-01-19 14:17:56 +0100

    🎉 STARTED project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+    name: 'Navbar',
+    components: {
+        RouterLink,
+        RouterView,
+    },
+}
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/contacts">Contacts</RouterLink>
+  </nav>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
