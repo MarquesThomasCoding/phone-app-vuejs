4eb38cc22c8a198671f3cb8b751ad7f9979fcb44
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-01-19 14:32:43 +0100

    ✨ ADD keyboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ClavierView from '../views/ClavierView.vue'
 import ContactsView from '../views/ContactsView.vue'
 
 const router = createRouter({
@@ -7,8 +7,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'clavier',
+      component: ClavierView
     },
     {
       path: '/contacts',
---
+++ src/views/ClavierView.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'ClavierView',
+    methods: {
+        appeler() {
+            console.log('appeler');
+        },
+    }
+}
+</script>
+
+<template>
+    <div>
+        <ul class="clavier">
+            <li v-for="i in 9">
+                <button @click="appeler()" class="btn btn-clavier">{{ i-1 }}</button>
+            </li>
+            <li>
+                <button @click="appeler()" class="btn btn-clavier">*</button>
+            </li>
+            <li>
+                <button @click="appeler()" class="btn btn-clavier">0</button>
+            </li>
+            <li>
+                <button @click="appeler()" class="btn btn-clavier">#</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.clavier {
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
+</style>
